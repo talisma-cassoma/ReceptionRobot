535eb57f69fe401f64f7a2900f42e28193317252
Talisma <talisma@Air-de-Talisma.mshome.net>
2022-10-17 20:11:33 +0100

    teste

--- mobile/App.js
+++ mobile/App.js
@@ -1,91 +1,134 @@
-import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import * as faceapi from 'face-api.js'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { FaceDetector, Camera, Permissions, Constants } from 'expo';
 
-const video = document.getElementById('video')
-
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(App)
-
-video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
-})
-
-export default function App() {
-  const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
-
-  if (!permission) {
-    // Camera permissions are still loading
-    return <View />;
+export default class App extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    faces: []
+  }
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
   }
 
-  if (!permission.granted) {
-    // Camera permissions are not granted yet
+  renderFaces = () =>
+    <View style={styles.facesContainer} pointerEvents="none">
+      {this.state.faces.map(this.renderFace)}
+    </View>
+
+  renderFace({ bounds, faceID, rollAngle, yawAngle }) {
     return (
-      <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+      <View
+        key={faceID}
+        transform={[
+          { perspective: 600 },
+          { rotateZ: `${rollAngle.toFixed(0)}deg` },
+          { rotateY: `${yawAngle.toFixed(0)}deg` },
+        ]}
+        style={[
+          styles.face,
+          {
+            ...bounds.size,
+            left: bounds.origin.x,
+            top: bounds.origin.y,
+          },
+        ]}>
+        <Text style={styles.faceText}>rollAngle: {rollAngle.toFixed(0)}</Text>
+        <Text style={styles.faceText}>yawAngle: {yawAngle.toFixed(0)}</Text>
       </View>
     );
   }
 
-  function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+
+  render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={styles.container}>
+          <Camera
+            style={styles.camera}
+            type={'front'}
+            onFacesDetected={this.handleFacesDetected}
+            faceDetectorSettings={{
+              mode: FaceDetector.Constants.Mode.fast,
+              detectLandmarks: FaceDetector.Constants.Mode.none,
+              runClassifications: FaceDetector.Constants.Mode.none,
+            }}>
+            <View style={styles.topBar}>
+              <Text style={styles.textcolor}>x: {this.state.faces.length ? this.state.faces[0].bounds.origin.x.toFixed(0) : 0}</Text>
+              <Text style={styles.textcolor}>y: {this.state.faces.length ? this.state.faces[0].bounds.origin.y.toFixed(0) : 0}</Text>
+            </View>
+            <View style={styles.bottomBar}>
+              <Text style={styles.textcolor}>Heigth: {this.state.faces.length ? this.state.faces[0].bounds.size.height.toFixed(0) : 0}</Text>
+              <Text style={styles.textcolor}>width: {this.state.faces.length ? this.state.faces[0].bounds.size.width.toFixed(0) : 0}</Text>
+            </View>
+          </Camera>
+          {this.state.faces.length ? this.renderFaces() : undefined}
+
+        </View>
+      );
+    }
   }
 
-  return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-            <Text style={styles.text}>é nos mais ninguem, sangra papa </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-    </View>
-  );
+  handleFacesDetected = ({ faces }) => {
+    if (faces.length > 0) {
+      this.setState({ faces });
+    }
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
   camera: {
     flex: 1,
+    justifyContent: 'space-between',
   },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
+  topBar: {
+    flex: 0.2,
     backgroundColor: 'transparent',
-    margin: 64,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: Constants.statusBarHeight + 1,
   },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
+  bottomBar: {
+    flex: 0.2,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
-  text: {
-    fontSize: 24,
+  face: {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 1,
+    position: 'absolute',
+    borderColor: '#808000',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  faceText: {
+    color: '#32CD32',
     fontWeight: 'bold',
-    color: 'white',
+    textAlign: 'center',
+    margin: 10,
+    backgroundColor: 'transparent',
   },
+  facesContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    top: 0,
+  },
+  textcolor: {
+    color: '#008080',
+  }
 });
-
